e79d473aaf0b604d45f312caf873c17640f696e5
James Lamb <jaylamb20@gmail.com>
2020-06-24 10:27:23 -0500

    fix flake8 things (#19)

--- dask_saturn/core.py
+++ dask_saturn/core.py
@@ -75,7 +75,7 @@ class SaturnCluster(SpecCluster):
         Destroy existing Dask cluster attached to the Jupyter Notebook or
         Custom Deployment and recreate it with the given configuration.
         """
-        print(f"Resetting cluster.")
+        print("Resetting cluster.")
         url = urljoin(BASE_URL, "api/dask_clusters/reset")
         cluster_config = {
             "n_workers": n_workers,

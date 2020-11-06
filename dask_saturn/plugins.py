603d2a9e28cd5b8769632e93339e4199a054764e
Julia Signell <jsignell@gmail.com>
2020-11-06 14:14:13 -0500

    Closer...

--- dask_saturn/plugins.py
+++ dask_saturn/plugins.py
@@ -5,7 +5,8 @@ import os
 import subprocess
 
 from distributed.comm import resolve_address
-from distributed.worker import get_client
+from distributed.worker import get_client, get_worker
+from distributed import worker_client
 
 
 class SaturnSetup:
@@ -21,34 +22,40 @@ class SaturnSetup:
 
 def update_files(client, path):
     tmp_path = None
+    path = os.path.abspath(path)
     if os.path.isdir(path):
+        path += '/'
         tmp_path = "/tmp/data.tar.gz"
-        subprocess.run(f"tar  --exclude .git -cvzf {tmp_path} -C {path} .", shell=True, check=True)
+        subprocess.run(f"tar --exclude .git -cvzf {tmp_path} -C {path} .", shell=True, check=True)
     with open(tmp_path or path, "rb") as f:
         payload = f.read()
     if path in list(client.datasets):
         client.unpublish_dataset(path)
     client.publish_dataset(**{path: payload})
-    client.run(_register_files, client, [path])
+    client.run(register_files, [path])
 
 
-def _register_files(client, paths):
-    for path in paths:
-        if path.endswith("/"):
-            isdir = True
-            tmp_path = "/tmp/data.tar.gz"
-        with open(tmp_path or path, "wb+") as f:
-            f.write(client.get(path))
-        if isdir:
-            subprocess.run(f"mkdir -p {path}", shell=True, check=True)
-            subprocess.run(f"tar -xvzf {tmp_path} -C {path}", shell=True, check=True)
+def register_files(paths=None):
+    """Register all files in the given paths on the workers"""
+    with worker_client(separate_thread=False) as client:
+        if paths is None:
+            paths = client.list_datasets()
+        for path in paths:
+            tmp_path = None
+            if path.endswith("/"):
+                isdir = True
+                tmp_path = "/tmp/data.tar.gz"
+            with open(tmp_path or path, "wb+") as f:
+                f.write(client.get_dataset(path))
+            if isdir:
+                subprocess.run(f"mkdir -p {path}", shell=True, check=True)
+                subprocess.run(f"tar -xvzf {tmp_path} -C {path}", shell=True, check=True)
     return os.listdir()
 
 
-class UploadFile:
+class UploadFiles:
     """WorkerPlugin for uploading files or directories to dask workers."""
 
     def setup(self, worker=None):
         """This method gets called at worker setup for new and existing workers"""
-        with get_client(worker) as c:
-            _register_files(c, list(c.datasets))
+        register_files()
